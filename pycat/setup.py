3853d84c1ca9b673c7972d79664a98fd3f4f5e23
Galen Boyer <gboyer@olin.edu>
2025-07-04 12:58:12 -0400

    ..

--- pycat/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="pycat",
-    version="0.1.0",
-    py_modules=["pycat"],
-    install_requires=[],
-)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pycat",
+    version="0.1.0",
+    packages=["pycat"],
+    install_requires=[
+        'os','argparse'
+    ],
+    description="A simple utility to concatenate things.",
+    author="Galen Boyer",
+    author_email="your@email.com",
+    url="https://github.com/galendboyer/pycat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
